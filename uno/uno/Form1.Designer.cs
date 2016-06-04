76e85e2a87deb125cf433d725468791b774fa1b4
OscarRuano <OscarRuano@OSCAR>
2016-06-04 10:05:23 -0600

    Mover Boton

--- uno/uno/Form1.Designer.cs
+++ uno/uno/Form1.Designer.cs
@@ -38,7 +38,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(127, 163);
+            this.button1.Location = new System.Drawing.Point(126, 186);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
